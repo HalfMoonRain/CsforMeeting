26d21a02b32b1fbf072bbc3750a14ef3089d2d53
JunWoo Lee <junwoo1357@gmail.com>
2023-07-13 23:29:45 +0900

    주석 추가

--- javatest.java
+++ javatest.java
@@ -1,5 +1,6 @@
-// public class javatest{ 
-//     public static void main(String[] args) {
-//          System.out.println("Hello World"); 
-//     } 
-// }
+// .class 확장자 vscode에서 exclude 해버렸음 
+public class javatest{ 
+    public static void main(String[] args) {
+         System.out.println("Hello World"); 
+    } 
+}
